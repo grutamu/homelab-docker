06690cac5f32bc66664448fbc1191d90ac47b52e
Zach Bardwell <4950357+grutamu@users.noreply.github.com>
2025-08-20 14:38:09 -0500

    (monitoring) initial commit

--- decrypt.sh
+++ decrypt.sh
@@ -6,3 +6,9 @@ op inject -i ./traefik/.env.tpl -o ./traefik/.env -f
 
 #frigate
 op inject -i ./frigate/config/config.yml.tpl -o ./frigate/config/config.yml -f
+
+#mediaserver stack
+op inject -i ./mediaserver/.env.tpl -o ./mediaserver/.env -f
+
+#monitoring stack
+op inject -i ./monitoring/.env.tpl -o ./monitoring/.env -f
